38454e00de73753bd9c5d7beddde15f0404d28fa
Junior Salzano <junior_salano@hotmail.com>
2025-01-21 00:57:01 -0300

    feat: adiciona controladora HomeController

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 $router->get('/', 'HomeController@index', false);
+$router->get('/home', 'HomeController@index', false);
 
 $router->get('/login', 'AuthController@login', false);
 $router->post('/login', 'AuthController@login', false);
