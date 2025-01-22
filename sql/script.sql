5d0411499ba19b56e26662e014b46c3420cf6a84
Junior Salzano <junior_salano@hotmail.com>
2025-01-22 11:08:46 -0300

    feat: adicina campo para controle de acesso de administrador e insert do primeiro usuario

--- sql/script.sql
+++ sql/script.sql
@@ -11,6 +11,21 @@ CREATE TABLE users (
   neighborhood VARCHAR(100),
   city VARCHAR(100),
   state VARCHAR(50),
+  access_level VARCHAR(1) NOT NULL DEFAULT 'U',
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Inserir o primeiro usuário administrador
+INSERT INTO users (name, email, password, zip_code, address, neighborhood, city, state, access_level)
+VALUES (
+  'Admin', 
+  'admin@example.com', 
+  '$2y$10$.6m3HOvAmUzgxsyiBhHM7OyFQ2sfHEE29BIgVTe3p404IrJbrh4Z6', --password: admin
+  '00000-000', 
+  'Admin Address', 
+  'Admin Neighborhood', 
+  'Admin City', 
+  'Admin State', 
+  'A'
 );
